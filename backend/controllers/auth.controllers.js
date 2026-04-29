f729ad5efab65bc8831492d2369b72b01f96263d
Your Name <priyanshushekhar019>
2026-04-29 23:44:26 +0530

    Fix cross-domain cookie authentication

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -20,8 +20,8 @@ export const signUp=async(req,res)=>{
         res.cookie("token",token,{
             httpOnly:true,
             maxAge:7*24*60*60*1000,
-            sameSite:"lax",
-            secure:false
+            sameSite:"none",
+            secure:true
         })
         return res.status(201).json(user);
     } catch (error) {
@@ -44,8 +44,8 @@ export const Login=async(req,res)=>{
         res.cookie("token",token,{
             httpOnly:true,
             maxAge:7*24*60*60*1000,
-            sameSite:"lax",
-            secure:false
+            sameSite:"none",
+            secure:true
         })
         return res.status(200).json(user);
     } catch (error) {
